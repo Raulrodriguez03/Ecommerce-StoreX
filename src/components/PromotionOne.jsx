a2e29fa2333fffbb6ac5d5467744837d55dadb7b
Raulrodriguez03 <rodriguezvalenciaraul03@gmail.com>
2023-08-05 13:52:51 -0600

    add Promo component

---
+++ src/components/PromotionOne.jsx
@@ -0,0 +1,22 @@
+export const PromotionOne = () => {
+  return (
+    <div className="promOne">
+      <text>
+        New arrival
+        <h1>2x1 in Basic Hoodies</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+          architecto aut minus officiis quaerat. Accusamus animi aspernatur
+          assumenda, at ex facere, fugiat hic ipsa porro quae qui ratione
+          temporibus unde?
+        </p>
+        <button>Buy Now</button>
+      </text>
+      <img
+        className="imagenProm"
+        src="https://assets.website-files.com/62f51a90d298e6f454bbffca/62f59f2e85742f2812aad006_image-1-promotion-shopwave-template.webp"
+        alt="camisas 2x1"
+      />
+    </div>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import { Navbar } from "../components/Navbar.jsx";
 import { CategoriesNav } from "../components/CategoriesNav.jsx";
+import { PromotionOne } from "../components/PromotionOne";
 
 export const Home = () => {
   return (
     <div className="home">
       <CategoriesNav />
+      <PromotionOne />
     </div>
   );
 };
